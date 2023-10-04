0e1454b1ba530dbfc8fce0cc0e3dfeda21f85ce8
Moshiur Rahman Maruf <84917344+creativemoshiur@users.noreply.github.com>
2023-10-04 22:53:37 +0600

    Add files via upload
    Final

---
+++ Product.php
@@ -0,0 +1,24 @@
+<?php
+
+class Product{
+    private int $id;
+    private string $name;
+    private float $price;
+    public function __construct(int $id,string $name,float $price){
+        $this->id=$id;
+        $this->name=$name;
+        $this->price=$price;
+    }
+    public function getFormattedPrice(){
+        return number_format($this->price,2);
+    }
+    public function showDetails(){
+        echo "Porduct Details: \n";
+        echo "- ID: {$this->id} \n";
+        echo "- Name: {$this->name} \n";
+        echo "- Price: \${$this->getFormattedPrice()} \n";
+    }
+}
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
+?>
